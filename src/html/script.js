8714da0d7f6f3872722f1312135aa9df9aea16c7
oocco <oocco@qq.com>
2019-09-24 13:46:24 +0800

    添加温度设置及显示

--- src/html/script.js
+++ src/html/script.js
@@ -28,17 +28,33 @@ var vuePanel = new Vue({
         panel: 'home', //Switch panel status
 
         //file panel value
-        path: '/',  //Folder path
+        path: '/', //Folder path
         fileList: [
             //{id: 1, type: 'file', name: '404.html', size: '2 KB'}
         ],
+
+        //temperature value
+        temperatureList: {
+            temperature: [-1,-1,-1,-1],
+            speed: [-1,-1,-1,-1],
+            status: -1,
+            espid: [-1,-1,-1,-1,-1]
+        },
+        temperatureValueLast: -1,
+        temperatureValue: -1,
+        temperatureSpeed: -1,
+        temperatureSpeedLast: -1,
+        
+        temperatureAutoStatus: false, //enable auto temperature control
+
+        temperatureRefreshTime: 3,
+        timeSender: null,
+
         flashInformation: {status: '', total: '', used: '', occupation: 0}, //File status
 
-        //Url setting
-        serverUri: '',
-
-        //device id
-        serialNumber: '',
+        serverUri: '', //Url setting
+        
+        serialNumber: '', //device id
 
         //Device type
         deviceType: [
@@ -55,6 +71,9 @@ var vuePanel = new Vue({
 
         //add device temp object
         deviceTempObject: {id: null, name: "",pin: -1 , type: 0, status: 0},
+
+        deviceRefreshTime: 12,
+        deviceTimeSender: null,
         
         //tips valus
         msg: '', //Alert message
@@ -85,16 +104,32 @@ var vuePanel = new Vue({
             }
             return '/validate';
         }, //validate ajax url path
+        temperatureUri: function(){
+            if(this.serverUri){
+                return this.serverUri + '/temperature'
+            }
+            return '/temperature';
+        } //temperature ajax url path
+        
     },
     created: function(){
         let _this = this;
         _this.initializeLcoalStorge();
+        
         //Get device information from Device
         _this.msg = "Refresh List ... ";
+        _this.getDevicesList();
+        _this.autoRefreshData();
+
+        //Get device information from Device
         let n = new FormData;
         n.append("list", "list");
-        _this.ajaxRequest(_this.deviceUri, n);
         _this.ajaxRequest(_this.fileUri, n);
+
+        //_this.temperatureRefresh();
+        _this.setAutoRefreshTime();
+
+        _this.getTemperatureList();
     },
     watch: {
         //Show alert message when msg has change
@@ -107,6 +142,20 @@ var vuePanel = new Vue({
                     _this.msgStatus = false;
                 }, 5000);
             }
+        },
+        temperatureAutoStatus: function(){
+            let _this = this;
+            let change = false;
+            if(_this.temperatureAutoStatus){
+                if(_this.temperatureList.status != 1){change = true;}
+                _this.temperatureList.status = 1;
+            }else{
+                if(_this.temperatureList.status == 1){change = true;}
+                _this.temperatureList.status = 0;
+            }
+            if(change){
+                _this.setTemperatureStatus();
+            }
         }
     },
     methods: {
@@ -116,6 +165,12 @@ var vuePanel = new Vue({
             _this.password = _this.getLocalStorage("password");
             _this.serverUri = _this.getLocalStorage("serverUri");
             _this.serialNumber = _this.getLocalStorage("serialNumber");
+            if(_this.getLocalStorage("temperatureRefreshTime") != null){
+                _this.temperatureRefreshTime = _this.getLocalStorage("temperatureRefreshTime");
+            }
+            if(_this.getLocalStorage("deviceRefreshTime") != null){
+                _this.deviceRefreshTime = _this.getLocalStorage("deviceRefreshTime");
+            }
         },
         //add password and send request
         ajaxRequest: function(uri, n, msg){
@@ -124,16 +179,53 @@ var vuePanel = new Vue({
             axios.post(uri, n)
             .then(function(response){
                 //refresh device list
-                if('deviceList' in response.data){
-                    _this.deviceList = response.data.deviceList;
-                    _this.delEmptyDevice();
-                    _this.msg = response.data.status;
+                if('temperatureValue' in response.data){
+                    _this.temperatureValue = response.data.temperatureValue;
+
+                    if(response.data.temperatureSpeed != _this.temperatureSpeedLast){
+                        _this.temperatureSpeed = response.data.temperatureSpeed/10;
+                        _this.temperatureSpeedLast = response.data.temperatureSpeed;
+
+                        for(let i=0; i<_this.temperatureList.espid.length; i++){
+
+                            for(let v=0; v<_this.deviceList.length; v++){
+                                if(_this.temperatureList.espid[i] == _this.deviceList[v].id){
+                                    _this.deviceList[v].status = _this.temperatureSpeedLast;
+                                }
+                            }
+
+                        }
+                        //_this.deviceRefreshDate();
+                    }
 
                 //refresh single device status, msg = id
                 }else if('deviceStatus' in response.data){
                     _this.setIdStatus(response.data.deviceStatus, msg);
+                
+                //refresh files list
+                }else if('deviceList' in response.data){
+                    _this.deviceList = response.data.deviceList;
+                    _this.delEmptyDevice();
+                    if(response.data.status != ""){
+                        _this.msg = response.data.status;
+                    }else{
+                        _this.msgStatus = false;
+                    }
+
+                //refresh temperature list
+                }else if('temperatureList' in response.data){
+                    _this.temperatureList = response.data.temperatureList;
+                    if(_this.temperatureList.status == 1){
+                        _this.temperatureAutoStatus = true;
+                    }else{
+                        _this.temperatureAutoStatus = false;
+                    }
+                    if(response.data.status != ""){
+                        _this.msg = response.data.status;
+                    }
 
                 //refresh files list
+                
                 }else if('files' in response.data){
                     _this.fileList = response.data.files;
                     _this.flashInformation.status = response.data.status;
@@ -154,11 +246,15 @@ var vuePanel = new Vue({
                 }else{
                     _this.msg = response.data.status;
                 }
+
                 setTimeout(function(){_this.ajaxCount = 0;}, 500);
+                
             })
             .catch(function(error){
                 _this.msg = "Send Failed! ";
                 if(error.status === 401){
+                    clearInterval(_this.timeSender);
+                    clearInterval(_this.deviceTimeSender);
                     _this.loginValidate = true;
                     //_this.loginAuth();
                 }else{
@@ -171,10 +267,10 @@ var vuePanel = new Vue({
         //device methods
         getDevicesList: function (){
             let _this = this;
-            _this.msg = "Get Device List ... ";
+            //_this.msg = "Get Device List ... ";
             let n = new FormData;
             n.append("list", "list");
-            ajaxRequest(_this.deviceUri, n);
+            _this.ajaxRequest(_this.deviceUri, n);
         },
         delEmptyDevice: function(){
             let _this = this;
@@ -211,7 +307,7 @@ var vuePanel = new Vue({
             }            
         },
         addDevice: function(){
-            _this = this;
+            let _this = this;
             for(let i=0; i < _this.deviceList.length; i++){
                 if(_this.deviceList[i].id === _this.deviceTempObject.id){
                     _this.msg = "Failed, Device have Used";
@@ -232,7 +328,7 @@ var vuePanel = new Vue({
             }
         },
         delDevice: function(id){
-            _this = this;
+            let _this = this;
             if(confirm("Confirm Delete Device? ID: " + id)){
                 let n = new FormData;
                 _this.msg = "Setting Device ... ";
@@ -290,6 +386,110 @@ var vuePanel = new Vue({
             }
         },
 
+        getDeviceName: function(id_in){
+            let _this = this;
+            for(let i=0; i<_this.deviceList.length; i++){
+                if(_this.deviceList[i].id == id_in){
+                    return _this.deviceList[i].name;
+                    break;
+                }
+            }
+            return "Empty";
+        },
+
+        //temperature methods
+        temperatureRefresh: function(){
+            let n = new FormData;
+            let _this = this;
+            n.append("get_temperature", "1");
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        getTemperatureList: function(){
+            let n = new FormData;
+            let _this = this;
+            n.append("action", "get_list");
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        setTemperatureList: function(){
+            let n = new FormData;
+            let _this = this;
+
+            let t = _this.temperatureList;
+            delete t.espid;
+
+            n.append("action", "set_list");
+            n.append("json", JSON.stringify(t));
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        setTemperatureId: function(){
+            let n = new FormData;
+            let _this = this;
+
+            let t = "{\"espid\":"+ JSON.stringify(_this.temperatureList.espid) +"}";
+            n.append("action", "set_espid");
+            n.append("json", t);
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        setTemperatureStatus: function(){
+            let n = new FormData;
+            let _this = this;
+
+            let t = "{\"status\":"+ JSON.stringify(_this.temperatureList.status) +"}";
+            n.append("action", "set_status");
+            n.append("json", t);
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        saveTemperatureList: function(){
+            let n = new FormData;
+            let _this = this;
+            _this.setTemperatureList();
+
+            n.append("action", "save");
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        resetTemperatureList: function(){
+            let n = new FormData;
+            let _this = this;
+            n.append("action", "reset");
+            _this.ajaxRequest(_this.temperatureUri, n);
+        },
+        setTemperatureEspid: function(id_in, index){
+            let _this = this;
+            if(id_in != -1){
+                _this.$set(_this.temperatureList.espid, index, -1);
+            }else{
+                let get_id = -1;
+                for(let i=0; i<_this.deviceList.length; i++){
+                    if (_this.deviceList[i].type == 3) {
+                        get_id = _this.deviceList[i].id;
+                        for(let v=0; v<_this.temperatureList.espid.length; v++){
+                            if(_this.deviceList[i].id == _this.temperatureList.espid[v]){
+                                get_id = -1;
+                                break;
+                            }
+                        }
+                    }
+                    if(get_id != -1){
+                        _this.$set(_this.temperatureList.espid, index, get_id);
+                        break;
+                    }
+                }
+            }
+        },
+
+        displayDeviceList: function(id_in){
+            let _this = this;
+            if(_this.temperatureAutoStatus){
+                for(let i=0; i<_this.temperatureList.espid.length; i++){
+                    if(_this.temperatureList.espid[i] == id_in){
+                        return false;
+                        break;
+                    }
+                }
+            }
+            return true;
+        },//disable PWM control when auto temperature
+        
         //file methods
         delFile: function(file){
             let _this = this;
@@ -345,6 +545,50 @@ var vuePanel = new Vue({
             _this.ajaxRequest(_this.deviceUri, n);
             _this.ajaxRequest(_this.fileUri, n);
             _this.setLocalStorage("password", _this.password);
+            _this.setAutoRefreshTime();
+            _this.getTemperatureList();
+        },
+
+        //auto refresh data
+        autoRefreshData: function(){
+            this.temperatureRefresh();
+        },
+        toggleAutoRefreshDate: function(){
+            let _this = this;
+            if(_this.temperatureRefreshTime == 0){
+                _this.temperatureRefreshTime = 3;
+            }else{
+                _this.temperatureRefreshTime = 0;
+            }
+            _this.setAutoRefreshTime();
+        },
+
+        deviceRefreshDate: function(){
+            this.getDevicesList();
+        },
+        toggleDeviceRefreshDate: function(){
+            let _this = this;
+            if(_this.deviceRefreshTime == 0){
+                _this.deviceRefreshTime = 12;
+            }else{
+                _this.deviceRefreshTime = 0;
+            }
+            _this.setAutoRefreshTime();
+        },
+
+        //change refresh time
+        setAutoRefreshTime: function(toggle = false){
+            let _this = this;
+            _this.setLocalStorage("temperatureRefreshTime", _this.temperatureRefreshTime);
+            _this.setLocalStorage("deviceRefreshTime", _this.deviceRefreshTime);
+            clearInterval(_this.timeSender);
+            clearInterval(_this.deviceTimeSender);
+            if(_this.temperatureRefreshTime != 0){
+                _this.timeSender = setInterval(function(){_this.autoRefreshData();}, _this.temperatureRefreshTime*1000);
+            }
+            if(_this.deviceRefreshTime != 0){
+                _this.deviceTimeSender = setInterval(function(){_this.deviceRefreshDate();}, _this.deviceRefreshTime*1000);
+            }
         }
     }
 })
